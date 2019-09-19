24b5e68c8785e9691bd294817c719a05e97c9cd3
BagrijRoman <bagrijroman@gmail.com>
2019-09-19 16:22:40 +0300

    update footer

--- src/components/layout/Footer/Footer.js
+++ src/components/layout/Footer/Footer.js
@@ -1,12 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 
 const Footer = () => (
   <footer className="app-footer">
     <div className="footer-content">
-      <h3>Footer component</h3>
-      <div className="bottom-line">
+      <div className="footer-navigation">
+        <div className="nav-section">
+          <div className="nav-section-header">Navigation</div>
+          <ul className="nav-section-list">
+            <Link to="#" className="nav-item">Home</Link>
+            <Link to="#" className="nav-item">About</Link>
+            <Link to="#" className="nav-item">Terms of service</Link>
+            <Link to="#" className="nav-item">Privacy policy</Link>
+          </ul>
+        </div>
 
+        <div className="nav-section">
+          <div className="nav-section-header">About us</div>
+          <ul className="nav-section-list">
+            <Link to="#" className="nav-item">Blog</Link>
+            <Link to="#" className="nav-item">Portfolio</Link>
+            <Link to="#" className="nav-item">FAQ</Link>
+          </ul>
+        </div>
+
+      </div>
+      <div className="bottom-line">
           <div className="copyright">
             Copyright © {new Date().getFullYear()} All Rights Reserved.
           </div>
@@ -17,7 +37,6 @@ const Footer = () => (
             <a className="social-link instagram" href="#"><i className="fa fa-instagram" /></a>
             <a className="social-link youtube" href="#"><i className="fa fa-youtube" /></a>
           </div>
-
       </div>
     </div>
   </footer>
