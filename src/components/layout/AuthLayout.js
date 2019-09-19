990db3775edba60569314e142add8335078a3a44
BagrijRoman <bagrijroman@gmail.com>
2019-09-19 18:44:08 +0300

    update babel config and extend header

--- src/components/layout/AuthLayout.js
+++ src/components/layout/AuthLayout.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import T from 'prop-types';
 
+import Header from './Header';
 import Footer from './Footer';
 
 const AuthLayout = ({ children }) => (
   <div className="auth-layout">
+    <Header/>
     <div className="layout-content">
       <h3>Auth layout</h3>
       {children}
