c6cfb3bb0bcc9d13f75d119271d533587dcb1c85
BagrijRoman <bagrijroman@gmail.com>
2019-09-23 18:26:10 +0300

    add sign up class

---
+++ src/containers/SignUp/SignUp.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import T from 'prop-types';
+
+class SignUp extends Component {
+  static propTypes = {
+
+  };
+
+  constructor(props) {
+    super(props);
+
+
+  }
+
+
+
+  render() {
+
+
+    return (
+      <h3>Sign up page</h3>
+    );
+
+  }
+}
+
+export default SignUp;
