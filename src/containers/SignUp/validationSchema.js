965c6619f824f684a5bb8f0cb504cc55d4b75a41
BagrijRoman <bagrijroman@gmail.com>
2019-09-23 20:09:11 +0300

    add sign up validation

---
+++ src/containers/SignUp/validationSchema.js
@@ -0,0 +1,12 @@
+import Joi from 'joi';
+
+import { errors } from '../../const';
+
+const schema = Joi.object().keys({
+  name: Joi.string().min(3).max(30).required().error(() => errors.NAME_SHOULD_BE_FROM_THREE_TO_THIRTY_SYMBOLS),
+  email: Joi.string().email().required().error(() => errors.VALID_EMAIL_SHOULD_BE_PROVIDED),
+  password: Joi.string().min(6).max(20).required().error(() => errors.PASSWORD_SHOULD_BE_FROM_SIX_TO_TWENTY_SYMBOLS),
+  rePassword: Joi.any().valid(Joi.ref('password')).required().error(() => errors.PASSWORD_CONFIRMATION_SHOULD_MATCH),
+});
+
+export default schema;
