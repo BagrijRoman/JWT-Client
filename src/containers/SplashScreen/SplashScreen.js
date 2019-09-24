f1d96e08e63f082cb0ec256b9aa02a1e30f4180e
BagrijRoman <bagrijroman@gmail.com>
2019-09-24 11:10:26 +0300

    add splash screen component base

---
+++ src/containers/SplashScreen/SplashScreen.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import T from 'prop-types';
+
+class SplashScreen extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    const {  } = this.props;
+
+    return (
+      <h3>Splash screen</h3>
+    );
+  }
+}
+
+export default SplashScreen;
