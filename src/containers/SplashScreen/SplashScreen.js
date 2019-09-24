73f745a071e59a3a8549388ae239d4bacf1e0891
BagrijRoman <bagrijroman@gmail.com>
2019-09-24 11:53:57 +0300

    add splash screen base

--- src/containers/SplashScreen/SplashScreen.js
+++ src/containers/SplashScreen/SplashScreen.js
@@ -1,19 +1,37 @@
 import React, { Component } from 'react';
 import T from 'prop-types';
+import { connect } from 'react-redux';
+
+import { dispatch } from '../../redux/store';
 
 class SplashScreen extends Component {
+  static propTypes = {
+    accounts: T.object.isRequired,
+  };
+
   constructor(props) {
     super(props);
 
+    // todo  here should be existing tokens check
+    // if token present - valid it, ang get user in and toggle loding flase
+    // if token invalid or not present - toggle loading false and user will be redirected to a singin in page
+
+    setTimeout(() => {
+      dispatch({
+        type: 'ACCOUNTS::SET_LOADING',
+        payload: false,
+      });
+
+    }, 5000);
   }
 
   render() {
-    const {  } = this.props;
+    const { accounts, children } = this.props;
 
-    return (
-      <h3>Splash screen</h3>
-    );
+    return accounts.loading ? <h3>Splash screen will be here</h3> : children;
   }
 }
 
-export default SplashScreen;
+const mapStateToProps = ({ accounts }) => ({ accounts });
+
+export default connect(mapStateToProps, null)(SplashScreen);
