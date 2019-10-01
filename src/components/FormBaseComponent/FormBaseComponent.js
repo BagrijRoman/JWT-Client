be41080b0cd49a12b494a28fc236ffa7b8f38785
BagrijRoman <bagrijroman@gmail.com>
2019-10-01 17:28:05 +0300

    add form base component class members and use it for sign in form

--- src/components/FormBaseComponent/FormBaseComponent.js
+++ src/components/FormBaseComponent/FormBaseComponent.js
@@ -1,7 +1,38 @@
 import React, { Component } from 'react';
+import { notificator } from '../../utils';
 
 class FormBaseComponent extends Component {
+  componentDidMount() {
+    this._isMounted = true;
+  }
 
+  componentWillUnmount() {
+    this._isMounted = false;
+  }
+
+  toggleLoading = (value) => {
+    const { _isMounted } = this;
+
+    if (_isMounted) {
+      this.setState({ loading: value });
+    }
+  };
+
+  onInputChange = (valueKey) => (e, data) => this.setState({ [valueKey]: data.value });
+
+  setError = (error) => {
+    const stateUpdates = { errors: {} };
+
+    if (error) {
+      const { message, key } =  error.details;
+      Object.assign(stateUpdates, { errors: { [key]: message } });  // todo message should be trnaslated
+      notificator.error(message); // todo message should be trnaslated
+    }
+
+    this.setState(stateUpdates);
+  };
+
+  resetErrors = () => this.setState({ errors: {} });
 }
 
 export default FormBaseComponent;
